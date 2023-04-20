1a2ced08c607b51883033b6a83da4b127d75d5d3
Ankit Maharjan <ankit.maharjan@swifttech.com>
2023-04-19 22:48:02 -0700

    changes in  env

--- common/index.js
+++ common/index.js
@@ -3,10 +3,7 @@
     module.exports = {
         mysqlHelper: require("./mysqlHelper"),
         hasher: require("./../controller/helper/hasher"),
-<<<<<<< HEAD
-        teacherValidateHelper: require("./../controller/helper/teacher_validationHelper")
-=======
+        teacherValidateHelper: require("./../controller/helper/teacher_validationHelper"),
         studentvalidateHelper: require("./../controller/helper/student_validationHelper")
->>>>>>> 7bc08cf3ce6f6eaa97d0b44dffca0b9716491c6d
     }
 })()
