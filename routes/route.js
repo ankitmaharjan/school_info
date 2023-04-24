c8cf4d81db60bdd6c3299b76d8bb29bfd314ade8
Ankit Maharjan <ankit.maharjan@swifttech.com>
2023-04-23 22:17:09 -0700

    route of book and author

--- routes/route.js
+++ routes/route.js
@@ -4,6 +4,12 @@
     const express = require("express");
     const router = express.Router();
     
+    const teacher_route = require("../routes/teacher_route/teacher_route");
+    const student_route = require("../routes/student_route/student_route");
+    const book_route = require("../routes/book_route/book_route");
 
+    router.use("/teacher",teacher_route);
+    router.use("/student",student_route);
+    router.use("/book",book_route);
     module.exports = router;
 })()
