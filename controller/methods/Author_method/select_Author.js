c0db5d784d84aea87e60bb5f9a6f75dff50a4bbb
chhatkulikapilronaldo <chhatkulikapil997@gmail.com>
2023-04-26 16:53:49 +0545

    filter used in sql

--- controller/methods/Author_method/select_Author.js
+++ controller/methods/Author_method/select_Author.js
@@ -3,7 +3,7 @@
     const select_author = require('../../sql/Sql_author/read_author')
     module.exports = async(req, res, next) => {
         try {
-            const output2=await select_author();
+            const output2=await select_author(req.query);
             
             
             res.status(200).send(output2);
--- controller/sql/Sql_author/read_author.js
+++ controller/sql/Sql_author/read_author.js
@@ -1,42 +1,134 @@
 "use strict";
 (() => {
-  module.exports = async () => {
+  module.exports = async (req) => {
     try {
       const sqlString = require("sqlString");
       const helper = require("../../../common/index");
       //const output=await helper.mysqlHelper.query("Select * from students where id="+search);
 
-      let baseQuery = `SELECT 
-        a.author_name,
-        a.address,
-        b.book_name,
-        b.publisher,
-        CONCAT(s.firstName, s.lastName) AS fullname,
-        t.username,
-        t.phone
-    FROM
-        books as b
-       INNER JOIN
-        students AS s ON b.student_id=s.id
-            INNER JOIN
-        teachers AS t ON b.teacher_id=t.id
-    INNER JOIN
+      //inner join
+      //   let baseQuery = `SELECT
+      //     a.author_name,
+      //     a.address,
+      //     b.book_name,
+      //     b.publisher,
+      //     b.price,
+      //     CONCAT(s.firstName, s.lastName) AS fullname,
+      //     t.username,
+      //     t.phone
+      // FROM
+      //     books as b
+      //    INNER JOIN
+      //     students AS s ON b.student_id=s.id
+      //         INNER JOIN
+      //     teachers AS t ON b.teacher_id=t.id
+      // INNER JOIN
+      // authors as a ON b.author_id=a.id
+
+      // where 1=1
+      // `;
+
+      //left join
+      //     let baseQuery = `SELECT
+      //     a.author_name,
+      //     a.address,
+      //     b.book_name,
+      //     b.publisher,
+      //     b.price,
+      //     CONCAT(s.firstName, s.lastName) AS fullname,
+      //     t.username,
+      //     t.phone
+      // FROM
+      //     books as b
+      //    LEFT JOIN
+      //     students AS s ON b.student_id=s.id
+      //         LEFT JOIN
+      //     teachers AS t ON b.teacher_id=t.id
+      // LEFT JOIN
+      // authors as a ON b.author_id=a.id
+
+      // where 1=1
+      // `;
+
+      //mysql case
+      //      let baseQuery =`SELECT book_name, price,
+      // CASE WHEN price >300 THEN 'The price is greater than 300'
+      // WHEN price=250 THEN 'The price is equal to 250'
+      // ELSE 'price below 300'
+      // END AS book_name_price
+      // FROM books`;
+      //collection of join
+      //     let baseQuery = `SELECT
+      //     a.author_name,
+      //     a.address,
+      //     a.phone,
+      //     b.book_name,
+      //     b.publisher,
+      //     b.price,
+      //     t.username,
+      //     t.phone,
+      //     CONCAT(s.firstName, ' ', s.lastName) AS fullname,
+      //     CASE
+      //         WHEN b.price > 300 THEN 'The price is greater than 300'
+      //         WHEN b.price = 250 THEN 'The price is equal to 250'
+      //         ELSE 'price below 300'
+      //     END AS book_name_price
+      // FROM
+      //     books AS b
+      //         INNER JOIN
+      //     students AS s ON b.student_id = s.id
+      //         RIGHT JOIN
+      //     teachers AS t ON b.teacher_id = t.id
+      //         LEFT JOIN
+      //     authors AS a ON b.author_id = a.id
+
+      // where 1=1
+      // `;
+      let baseQuery = `SELECT
+      a.author_name,
+      a.address,
+      b.book_name,
+      b.publisher,
+      b.price,
+      CONCAT(s.firstName,' ', s.lastName) AS fullname,
+      t.username,
+      t.phone
+  FROM
+      books as b
+     LEFT JOIN
+      students AS s ON b.student_id=s.id
+          LEFT JOIN
+      teachers AS t ON b.teacher_id=t.id
+     LEFT JOIN
     authors as a ON b.author_id=a.id
-    
-    where 1=1
-    `;
+`;
+
+      if (req.filter.price) { // filter by price
+        let filterQuery = ` where 1=1 and b.price= ${req.filter.price}`;
+        baseQuery += filterQuery;
+      }
+
+      if (req.filter.id) {  //filter by id
+        let filterQuery = ` where 1=1 and s.id= ${req.filter.id}`;
+        baseQuery += filterQuery;
+      }
+      // if (req.filter.uuid) {  //filter by uuid
+      //   let filterQuery = ` where 1=1 and b.uuid= ${req.filter.uuid}`
+      //   baseQuery += filterQuery;
+      // }
 
       let formatedQUery = sqlString.format(baseQuery);
-      const output = await helper.mysqlHelper.query(formatedQUery);
-
-      // const [mainQuery] = await helper.mysqlHelper.query(baseQuery);
-
-      // let filterQuery = ` AND  where phone= ${mainQuery.filter.phone}`;
-
+      const [output] = await helper.mysqlHelper.query(formatedQUery); //destructuring
+      
+      //   const [mainQuery] = await helper.mysqlHelper.query(baseQuery);
+      //  const prices=(mainQuery.map((item)=>{
+      //   return(
+      //       item.price
+      //     )
+      //   }))
+      //let filterQuery = ` AND  where a.phone= ${mainQuery.filter.phone}`;
       // mainQuery = baseQuery + filterQuery;
-
-      // console.log(mainQuery);
-
+      //eturn filterQuery;
       return output;
     } catch (error) {
       console.log(error);
